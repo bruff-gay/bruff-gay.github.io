d0af6e93994a25f677a5c79051d9a3b23aa96a79
bruff-gay <github@bruff.gay>
2026-03-19 14:19:30 -0600

    Update wander.js

--- wander/wander.js
+++ wander/wander.js
@@ -1,9 +1,29 @@
 window.wander = {
-  // Other Wander consoles that visitors can reach from my console.
   consoles: [
-    'https://susam.net/wander/',
+    "https://4thguy.codeberg.page/wander/",
+    "https://arkholt.com/wander/",
+    "https://astandke.com/wander/",
+    "https://benovermyer.com/wander/",
+    "https://bitzero.cloud/wander/",
+    "https://bjro.dev/wander/",
+    "https://dahlstrand.net/wander/",
+    "https://fractaldragon.net/wander/",
+    "https://heitorpb.github.io/wander/",
+    "https://huffstler.github.io/wander/",
+    "https://hugo0.com/wander",
+    "https://joshing.you/wander/",
+    "https://juleskourelakos.com/wander/",
+    "https://martincapodici.com/wander/",
+    "https://matkelly.com/wander/",
+    "https://mkprc.xyz/wander/",
+    "https://susam.net/wander/",
+    "https://tony-zorman.com/wander/",
+    "https://tuftel.today/wander/",
+    "https://www.acdw.net/wander/",
+    "https://www.carlwarnick.com/wander/",
+    "https://www.davidtran.me/wander/",
+    "https://www.siddharthagolu.com/wander/"
   ],
-  // My favourite websites and pages I recommend to the Wander community.
   pages: [
     'https://midnight.pub/',
     'https://susam.net/',
